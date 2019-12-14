7ceaeadbc9cb4480a5460532b5c2c52d02da3eb2
Naveen <naveendevinda163@gmail.com>
2019-12-14 11:13:11 +0530

    create_new_flutter_project

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Kelaniya());
+}
+
+class Kelaniya extends StatelessWidget {
+  const Kelaniya({Key key}) :super(key:key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Raja"),),
+      body: Center(
+        child: RaisedButton(
+          child:Text("Press"),
+          onPressed: (){
+            
+          },
+        ),
+      ),
+    );
+  }
+}
+/*
+home: Center (
+        child:RaisedButton(
+          onPressed: null,
+          child: Text(
+            "Hi Kelaniya",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        
+    )
+Widget build(BuildContext context) {
+  return Center(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        const RaisedButton(
+          onPressed: null,
+          child: Text(
+            'Disabled Button',
+            style: TextStyle(fontSize: 20)
+          ),
+        ),
+        const SizedBox(height: 30),
+        RaisedButton(
+          onPressed: () {},
+          child: const Text(
+            'Enabled Button',
+            style: TextStyle(fontSize: 20)
+          ),
+        ),
+        const SizedBox(height: 30),
+        RaisedButton(
+          onPressed: () {},
+          textColor: Colors.white,
+          padding: const EdgeInsets.all(0.0),
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: <Color>[
+                  Color(0xFF0D47A1),
+                  Color(0xFF1976D2),
+                  Color(0xFF42A5F5),
+                ],
+              ),
+            ),
+            padding: const EdgeInsets.all(10.0),
+            child: const Text(
+              'Gradient Button',
+              style: TextStyle(fontSize: 20)
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+ */
